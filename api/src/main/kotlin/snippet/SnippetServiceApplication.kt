5e067603a98813d58cc57ea0fc2ab55032b2391b
Franco <fdescalzo@mail.austral.edu.ar>
2025-11-05 23:05:29 -0300

    chore: update Gradle build configuration and dependencies

--- api/src/main/kotlin/snippet/SnippetServiceApplication.kt
+++ api/src/main/kotlin/snippet/SnippetServiceApplication.kt
@@ -1,10 +1,8 @@
 package snippet
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication
 @ComponentScan(basePackages = ["snippet", "events"])
 class SnippetServiceApplication
 
