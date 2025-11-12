26af8eface44af2be9801c51d2c6178eaa9e2d19
Tomas093 <tjmonteiro@mail.austral.edu.ar>
2025-11-12 15:59:12 -0300

    refactor: update main class path and reorganize module dependencies in SnippetServiceApplication

--- api/src/main/kotlin/api/SnippetServiceApplication.kt
+++ api/src/main/kotlin/api/SnippetServiceApplication.kt
@@ -8,7 +8,37 @@ import org.springframework.boot.runApplication
         "events", "common", "security",
         "authorization", "consumers", "handlers",
         "config", "producers", "api", "assets",
-        "repositories", "component", "component",
+        "repositories", "component", "filters",
+        "controllers", "services",
+        // Módulo 'api'
+        "api",
+
+        // Módulo 'events'
+        "config",
+        "consumers",
+        "events",
+        "handlers",
+        "producers",
+        "requests",
+
+        // Módulo 'common'
+        "repositories",
+
+        // Módulos 'printscript' y 'assets'
+        "component",
+
+        // Módulos 'printscript' y 'snippets'
+        "services",
+
+        // Módulo 'security'
+        "security",
+
+        // Módulo 'authorization'
+        "authorization",
+
+        // Módulo 'snippets'
+        "controllers",
+        "filters",
     ],
 )
 class SnippetServiceApplication
